9473f244bf42fc4e1b0bac46d53d7fa7e65ec367
limangguo-geek <167859081+limangguo-geek@users.noreply.github.com>
2024-07-24 16:51:39 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-$(document).ready(function(){$('.testimonials-slider, .pricing-slider').slick({infinite:true,slidesToShow:3,slidesToScroll:1,autoplay:true,autoplaySpeed:2000,responsive:[{breakpoint:768,settings:{slidesToShow:1,slidesToScroll:1}}]});$('.faq-question').click(function(){$(this).next('.faq-answer').slideToggle();$(this).toggleClass('active')})});
+$(document).ready(function(){
+    $('.testimonials-slider, .pricing-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    
+    $('.faq-question').click(function() {
+        $(this).next('.faq-answer').slideToggle();
+        $(this).toggleClass('active');
+    });
+});
